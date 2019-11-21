3339aabb6e434b78be5b3f79a4a1e09931edc9d6
Ricardo Pérez <ricpelo@gmail.com>
2019-11-21 19:30:50 +0100

    Sube mastermind.py

---
+++ mastermind.py
@@ -0,0 +1,11 @@
+inventar un número de 4 dígitos
+si el jugador aún no ha agotado sus 10 intentos:
+    pedirle el siguiente intento al jugador
+    comprobar cuántos dígitos tiene correctamente puestos
+    si son todos:
+        ¡Ganó!
+        fin
+    si no:
+        decirle al jugador cuántos dígitos tiene correctamente puestos
+        comprobar cuántos dígitos están pero no en su sitio
+        decírselo al jugador
