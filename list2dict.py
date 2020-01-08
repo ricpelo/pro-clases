f87487d7eb69e8d204533b60b4717851ea715cfe
Ricardo Pérez <ricpelo@gmail.com>
2020-01-08 16:29:32 +0100

    Mete list2dict.py

--- list2dict.py
+++ list2dict.py
@@ -29,3 +29,30 @@ def dict2list(dic):
         lista.append(dic.get(i))
     return lista
 
+def dict2list_alt(dic):
+    """
+    >>> dict2list_alt({2: 'c', 1: 'b', 0: 'a'})
+    ['a', 'b', 'c']
+    >>> dict2list_alt({0: 'a', 2: 'c'})
+    ['a', None, 'c']
+    """
+    m = max(dic.keys())
+    lista = []
+    for i in range(m + 1):
+        lista.append(None)
+    for k, v in dic.items():
+        lista[k] = v
+    return lista
+
+
+def total_volume(*lista):
+    """
+    >>> total_volume([1, 2, 3])
+    6
+    >>> total_volume([1, 2, 3], [4, 5, 2])
+    46
+    """
+    from functools import reduce
+    def prod(lista):
+        return reduce(lambda x, y: x * y, lista, 1)
+    return sum(map(prod, lista))
