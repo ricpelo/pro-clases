a52f913fc806dc8ff58321e71cee057328d3cdc8
Ricardo Pérez <ricpelo@gmail.com>
2020-02-11 21:23:39 +0100

    Primer commit con Java

---
+++ java/Prueba.java
@@ -0,0 +1,10 @@
+public class Prueba {
+    public static void main(String[] args) {
+        int numero = 45;
+
+        System.out.println("Hola, mundo");
+        System.out.println(numero);
+        numero = 33;
+        System.out.println(numero);
+    }
+}
