4e92e7693ff685f2cc84f0cfd750992201a8a9f0
Ricardo Pérez <ricpelo@gmail.com>
2020-02-12 17:16:41 +0100

    Metemos la clase Contador

--- java/Prueba.java
+++ java/Prueba.java
@@ -1,10 +1,7 @@
 public class Prueba {
     public static void main(String[] args) {
-        int numero = 45;
-
+        Contador c = new Contador();
+        Contador z = c;
         System.out.println("Hola, mundo");
-        System.out.println(numero);
-        numero = 33;
-        System.out.println(numero);
     }
 }
