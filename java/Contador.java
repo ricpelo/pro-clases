8e51612576b2a74f13914579d74b1971c2d1b4f9
Ricardo Pérez <ricpelo@gmail.com>
2020-02-14 19:35:37 +0100

    Viernes

--- java/Contador.java
+++ java/Contador.java
@@ -1,3 +1,20 @@
 public class Contador {
-    
+    private int valor;
+
+    public int getValor() {
+        return this.valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+
+        return this.getValor() == ((Contador) obj).getValor();
+    }
 }
--- java/Prueba.java
+++ java/Prueba.java
@@ -1,7 +1,14 @@
 public class Prueba {
     public static void main(String[] args) {
         Contador c = new Contador();
-        Contador z = c;
-        System.out.println("Hola, mundo");
+        Contador z = new Contador();
+
+        System.out.println(c.equals(z));
+        System.out.println(c == z);
+        // // c.setValor(4);
+        // // z.setValor(4);
+        // System.out.println(c.getValor());
+        // System.out.println(z.getValor());
+        // System.out.println(c == z);
     }
 }
