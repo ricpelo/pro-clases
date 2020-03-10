27b926254ee63450913077a3c37e9832e58791bf
Ricardo Pérez <ricpelo@gmail.com>
2020-03-10 21:22:31 +0100

    Martes

--- java/Tabla.java
+++ java/Tabla.java
@@ -1,3 +1,5 @@
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 /**
@@ -6,22 +8,30 @@ import java.util.Scanner;
 public class Tabla {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        final int numero;
-        int i;
+        int numero;
 
-        System.out.print("Tabla a mostrar: ");
-        numero = scanner.nextInt();
-
-        if (numero >= 0 && numero <= 10) {
-            for (i = 0; i <= 10; i++) {
-                System.out.print(numero);
-                System.out.print(" x ");
-                System.out.print(i);
-                System.out.print(" = ");
-                System.out.println(numero * i);
+        for (;;) {
+            try {
+                System.out.print("Tabla a mostrar: ");
+                numero = scanner.nextInt();
+                if (numero >= 0 && numero <= 10) {
+                    break;
+                }
+                System.err.println("El número debe estar comprendido entre 0 y 10.");                
+            } catch (InputMismatchException e) {
+                System.err.println("Debe introducir un número entero.");
+                scanner.nextLine();
+            } catch (NoSuchElementException e) {
+                return;
             }
-        } else {
-            System.err.println("El número debe estar comprendido entre 0 y 10.");
+        }
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.print(numero);
+            System.out.print(" x ");
+            System.out.print(i);
+            System.out.print(" = ");
+            System.out.println(numero * i);
         }
     }
 }
